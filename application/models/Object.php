bd7becf7ea84db78ac5977f4bf83670123393d15
Pamela Viloria <pamelajaneviloria@gmail.com>
2024-02-19 09:27:17 +0800

    Initial commit

---
+++ application/models/Object.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Object extends CI_Model {
+	
+    public function __construct() {
+        parent::__construct();
+        // $this->load->library('form_validation');
+    }
+}
